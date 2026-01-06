15c53e54b370d2e3162cafedc3ca2109c8fc09ee
Mayorwa <mayorviper.mo@gmail.com>
2026-01-06 18:13:06 +0000

    27.Remove Element optimized

---
+++ leetcode/RemoveElement-27.py
@@ -0,0 +1,11 @@
+def removeElement( nums, val):
+    i = 0
+    for idx in nums:
+        if idx != val:
+            nums[i] = idx
+            i += 1
+    print(nums)
+    return i
+
+print(removeElement([3,2,2,3], 3)) # [2,2]
+print(removeElement([0,1,2,2,3,0,4,2], 2))  # [0,1,4,0,3]
