06fc57fc41b048ffdb0831b7c48509fa566e4540
Mayorwa <mayorviper.mo@gmail.com>
2026-01-13 16:52:32 +0000

    remove duplicates #26 first try passed

--- leetcode/RemoveDuplicates-26.py
+++ leetcode/RemoveDuplicates-26.py
@@ -1,5 +1,16 @@
 def removeDuplicates(nums) -> int:
-    return 2
+    k = 0
+    last_seen = '_'
+
+    for i in nums:
+        if i != last_seen:
+            nums[k] = i
+            k += 1
+            last_seen = i
+
+    return k
+
+
 
 print(removeDuplicates([1, 1, 2]))
 print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
