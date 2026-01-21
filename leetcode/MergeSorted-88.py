34f0c5c5173419f8de07f31e8a94c7a80c1c83cb
Mayorwa <mayorviper.mo@gmail.com>
2026-01-21 12:22:08 +0000

    Merge Sorted 88

---
+++ leetcode/MergeSorted-88.py
@@ -0,0 +1,8 @@
+def merge(nums1, m, nums2, n) -> None:
+    for i in range(0, len(nums1)):
+        print("i", i)
+        # if nums1[i] >
+
+
+print(merge([4, 5, 6, 0, 0, 0, 0], 3, [1,2,3, 4], 4))
+
