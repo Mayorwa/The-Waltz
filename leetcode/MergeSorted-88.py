eab26f9ff943159bf5f0de7cd76084111c45c70b
Mayorwa <mayorviper.mo@gmail.com>
2026-01-27 15:28:10 +0000

    brute force: backward pointer solution with if statements

--- leetcode/MergeSorted-88.py
+++ leetcode/MergeSorted-88.py
@@ -1,20 +1,24 @@
-def merge(nums1, m, nums2, n) -> None:
-    idx, i, p = 0
-    while idx < len(nums1):
-        if nums1[i] == 0:
-            nums1[i] = nums2[p]
-            i += 1
-            p += 1
-        elif nums1[i] < nums2[p]:
-            i += 1
+def merge(nums1, m, nums2, n):
+    idx = len(nums1) - 1
+    i = m - 1
+    j = n - 1
 
-        elif nums2[p] <= nums1[i]:
-            x = nums2[p]
-            nums2[p] = nums1[i]
-            nums1[i] = x
-            i += 1
-            
-        idx += 1
+
+    while idx >= 0:
+        if i < 0:
+            nums1[idx] = nums2[j]
+            j -= 1
+        elif j < 0:
+            nums1[idx] = nums1[i]
+            i -= 1
+        elif nums1[i] > nums2[j]:
+            nums1[idx] = nums1[i]
+            i -= 1
+        elif nums1[i] <= nums2[j]:
+            nums1[idx] = nums2[j]
+            j -= 1
+        idx -= 1
+    return nums1
 print(merge([4, 5, 6, 0, 0, 0, 0], 3, [1,2,3, 4], 4))
 
 print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
