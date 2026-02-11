cb0d83383c72e4934f2e9bf69df498efaadd5cea
Mayorwa <mayorviper.mo@gmail.com>
2026-02-11 11:12:13 +0000

    1374. Generate a String With Characters That Have Odd Counts

--- leetcode/GenerateString-1374.py
+++ leetcode/GenerateString-1374.py
@@ -1,8 +1,14 @@
 def generateTheString(n: int) -> str:
-    return ""
+    # check if number is odd or even
+    # if odd just return a string n times
+    # else: return a + b *n - 1
+    if n % 2 == 0:
+        return "a" + ("b" * (n - 1))
+    return "a" * n
 
 print("testcase 1: ", generateTheString(4))
 
 print("testcase 2: ", generateTheString(2))
 
 print("testcase 3: ", generateTheString(7))
+print("testcase 4: ", generateTheString(0))
