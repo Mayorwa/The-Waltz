72bcbc886f8fe3b09fec879ef4032719a017cfec
Mayorwa <mayorviper.mo@gmail.com>
2026-02-11 11:05:06 +0000

    GenerateString: 1374

---
+++ leetcode/GenerateString-1374.py
@@ -0,0 +1,8 @@
+def generateTheString(n: int) -> str:
+    return ""
+
+print("testcase 1: ", generateTheString(4))
+
+print("testcase 2: ", generateTheString(2))
+
+print("testcase 3: ", generateTheString(7))
