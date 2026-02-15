4dd5702e64928199dc05cb79bba9ee2657ab763e
Mayorwa <mayorviper.mo@gmail.com>
2026-02-15 13:28:40 +0000

    String Manipulation questions

--- leetcode/ExcelSheetColumn-168.py
+++ leetcode/ExcelSheetColumn-168.py
@@ -1,6 +1,7 @@
 def convertToTitle(columnNumber):
     # approach 1:
     # alpha* 26^n + beta * 26^n-1 + gamma + 26^n-2
+    # while(n > 0) n modulo 26 get remainder and set n to be equals to n//26
     return "AA"
 
 print("Testcase 1", convertToTitle(1)) #A
